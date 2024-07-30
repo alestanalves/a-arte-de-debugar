9448b55a71b126a8293d39d9dec300ed9a799e5f
alestanalves <alestanalves@gmail.com>
2024-07-30 01:59:25 -0300

    add new favicon

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { CheckIcon } from "@radix-ui/react-icons"
-
 import { cn } from "@/lib/utils"
 
 const Checkbox = React.forwardRef<
